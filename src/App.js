296d4fc17ab157b42c1f8cf83cf57ac9fdc03844
tanpm <tanpm@mobiletech.vn>
2022-06-14 21:55:06 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TestA from "./components/TestComponent/TestA";
+import React, { Component } from "react";
 function App() {
+  // state or functional component React.Element
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,19 +12,45 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React composite
-        </a>
-        <TestA />
+        <div>Count: 0 </div>
+        <button>count</button>
+        {/* <Time currentTime={10} />
+        <Time></Time>
+        <TestA /> */}
+        <Chair number={1} name={"Anh"} />
+        <Chair number={2} />
       </header>
     </div>
   );
 }
-let test = true;
+
+function Time(props) {
+  return <div>curreet is: {props.currentTime}</div>;
+}
+
+function Chair(props) {
+  // return React.Element
+  return (
+    <div>
+      chair {props.number} + {props.name}
+    </div>
+  );
+}
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let anh = new Person("anh", 29);
+let em = new Person("em", 17);
+
+//
+// factory.
 
 export default App;
+
+// tao ra 2 function component va su dung chung:
+//
